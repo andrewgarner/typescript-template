522e46c826a46a449a0f04d9c977c0001ad50c84
Andrew Garner <andrew@andrewgarner.com>
2025-01-14 17:14:01 +0000

    Migrate Prettier config to JS format

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  plugins: ["prettier-plugin-packagejson"],
+};
+
+export default config;
